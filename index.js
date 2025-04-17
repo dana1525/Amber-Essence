ebd70b54459be59f0e5dcba1da92f29ccf40f851
dana1525 <mirescudany@gmail.com>
2025-04-17 22:44:12 +0300

    part5

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express= require("express");
 const path= require("path");
 const fs = require("fs");
-// const sharp = require("sharp");
-// const sass = require("sass");
+const sharp = require("sharp");
+const sass = require("sass");
 // const pg = require("pg");
 
 app= express();
@@ -14,22 +14,79 @@ console.log("Folderul curent de lucru: ", process.cwd())
 app.set("view engine", "ejs");
 
 obGlobal={
-    obErori:null,
-    obImagini:null,
-    // folderScss: path.join(__dirname, "resurse/scss"),
-    // folderCss: path.join(__dirname, "resurse/css"),
-    // folderBackup: path.join(__dirname, "backup")
+    obErori: null,
+    obImagini: null,
+    folderScss: path.join(__dirname, "resurse/scss"),
+    folderCss: path.join(__dirname, "resurse/css"),
+    folderBackup: path.join(__dirname, "backup")
 }
 
 vect_foldere=["temp", "backup", "temp1"]
 //let -> variabile locale, vizibile strict in folder
-//Sync -> functie sincrona
 for(let folder of vect_foldere){
     let caleFolder = path.join(__dirname, folder)
     if (!fs.existsSync(caleFolder)){
         fs.mkdirSync(caleFolder)
     }
 }
+
+function compileazaScss(caleScss, caleCss){
+    console.log("cale:",caleCss);
+    if(!caleCss){
+
+        let numeFisExt = path.basename(caleScss);
+        let numeFis = numeFisExt.split(".")[0]
+        caleCss = numeFis + ".css";
+    }
+    
+    if (!path.isAbsolute(caleScss))
+        caleScss = path.join(obGlobal.folderScss, caleScss)
+    if (!path.isAbsolute(caleCss))
+        caleCss = path.join(obGlobal.folderCss, caleCss)
+    
+
+    let caleBackup = path.join(obGlobal.folderBackup, "resurse/css");
+    if (!fs.existsSync(caleBackup)) {
+        fs.mkdirSync(caleBackup,{recursive:true})
+    }
+    
+    let numeFisCss=path.basename(caleCss);
+    let numeFisExt = numeFisCss.split(".")[0];  ///
+    let ext = path.extname(numeFisCss);         ///
+    try{
+        if (fs.existsSync(caleCss)){
+            let timestamp = (new Date()).getTime();                 ////
+            let numeFisBackup = `${numeFisExt}_${timestamp}${ext}`; ////
+            fs.copyFileSync(caleCss, path.join(obGlobal.folderBackup, "resurse/css", numeFisCss))  ////
+        }
+    }catch(err){
+        console.err("Eroare la copierea fisierului", err.message);
+    }
+    rez = sass.compile(caleScss, {"sourceMap":true});
+    fs.writeFileSync(caleCss, rez.css)
+}
+
+
+
+vFisiere=fs.readdirSync(obGlobal.folderScss);
+for( let numeFis of vFisiere ){
+    if (path.extname(numeFis)==".scss"){
+        compileazaScss(numeFis);
+    }
+}
+
+
+fs.watch(obGlobal.folderScss, function(eveniment, numeFis){
+    console.log(eveniment, numeFis);
+    if (eveniment=="change" || eveniment=="rename"){
+        let caleCompleta=path.join(obGlobal.folderScss, numeFis);
+        if (fs.existsSync(caleCompleta)){
+            compileazaScss(caleCompleta);
+        }
+    }
+})
+
+
 
 function initErori(){
     let continut = fs.readFileSync(path.join(__dirname,"resurse/json/erori.json")).toString("utf-8");
@@ -39,7 +96,7 @@ function initErori(){
     
     obGlobal.obErori.eroare_default.imagine=path.join(obGlobal.obErori.cale_baza, obGlobal.obErori.eroare_default.imagine)
     for (let eroare of obGlobal.obErori.info_erori){
-        eroare.imagine=path.join(obGlobal.obErori.cale_baza, eroare.imagine)
+        eroare.imagine = path.join(obGlobal.obErori.cale_baza, eroare.imagine)
     }
     console.log(obGlobal.obErori)
 
@@ -48,27 +105,58 @@ function initErori(){
 initErori()
 
 
-///imagini
+function initImagini(){
+    var continut= fs.readFileSync(path.join(__dirname,"resurse/json/galerie.json")).toString("utf-8");
+
+    obGlobal.obImagini = JSON.parse(continut);
+    let vImagini = obGlobal.obImagini.imagini;
+
+    let caleAbs = path.join(__dirname,obGlobal.obImagini.cale_galerie);
+    let caleAbsMediu = path.join(__dirname,obGlobal.obImagini.cale_galerie, "mediu");
+    let caleAbsMic = path.join(__dirname,obGlobal.obImagini.cale_galerie, "mic");
+    if (!fs.existsSync(caleAbsMediu))
+        fs.mkdirSync(caleAbsMediu);
+
+    if (!fs.existsSync(caleAbsMic))
+        fs.mkdirSync(caleAbsMic);
+
+    for (let imag of vImagini){
+        [numeFis, ext] = imag.cale_fisier.split(".");
+        let caleFisAbs = path.join(caleAbs,imag.cale_fisier);
+        let caleFisMediuAbs = path.join(caleAbsMediu, numeFis+".webp");
+        let caleFisMicAbs = path.join(caleAbsMic, numeFis+".webp");
+        sharp(caleFisAbs).resize(300).toFile(caleFisMediuAbs);
+        sharp(caleFisAbs).resize(150).toFile(caleFisMicAbs);
+        imag.fisier_mediu = path.join("/", obGlobal.obImagini.cale_galerie, "mediu", numeFis + ".webp" );
+        imag.fisier_mic = path.join("/", obGlobal.obImagini.cale_galerie, "mic", numeFis + "webp");
+        imag.fisier = path.join("/", obGlobal.obImagini.cale_galerie, imag.cale_fisier );
+        
+    }
+    console.log(obGlobal.obImagini)
+}
+
+initImagini();
+
 
 
 function afisareEroare(res, identificator, titlu, text, imagine){
     let eroare= obGlobal.obErori.info_erori.find(function(elem){ 
-                        return elem.identificator==identificator
+                        return elem.identificator == identificator
                     });
     if(eroare){
         if(eroare.status)
             res.status(identificator)
-        var titluCustom=titlu || eroare.titlu;
-        var textCustom=text || eroare.text;
-        var imagineCustom=imagine || eroare.imagine;
+        var titluCustom = titlu || eroare.titlu;
+        var textCustom = text || eroare.text;
+        var imagineCustom = imagine || eroare.imagine;
 
 
     }
     else{
-        var err=obGlobal.obErori.eroare_default
-        var titluCustom=titlu || err.titlu;
-        var textCustom=text || err.text;
-        var imagineCustom=imagine || err.imagine;
+        var err = obGlobal.obErori.eroare_default
+        var titluCustom = titlu || err.titlu;
+        var textCustom = text || err.text;
+        var imagineCustom = imagine || err.imagine;
 
 
     }
@@ -90,8 +178,7 @@ app.get("/favicon.ico", function(req, res){
 })
 
 app.get(["/","/index","/home"], function(req, res){
-    res.render("pagini/index", {ip: req.ip});
-    //, imagini: obGlobal.obImagini.imagini
+    res.render("pagini/index", {ip: req.ip, imagini: obGlobal.obImagini.imagini});
 })
 
 // app.get("/despre", function(req, res){
@@ -100,6 +187,14 @@ app.get(["/","/index","/home"], function(req, res){
 
 app.get("/tips-n-tricks", function(req, res){
     res.render("pagini/tips-n-tricks");
+})
+
+app.get("/galerie", function(req, res){
+    res.render("pagini/galerie", {imagini: obGlobal.obImagini.imagini});
+})
+
+app.get("/retete", function(req, res){
+    res.render("pagini/retete");
 })
 
 app.get("/index/a", function(req, res){
